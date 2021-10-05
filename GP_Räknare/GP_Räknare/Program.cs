b92552e647b3ae22c31047dfcf8b37c6e83ee441
nisse27a <71439623+nisse27a@users.noreply.github.com>
2021-10-05 13:31:34 +0200

    Började speedcalculator

--- GP_Räknare/GP_Räknare/Program.cs
+++ GP_Räknare/GP_Räknare/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace GP_Räknare
 {
@@ -10,6 +11,24 @@ namespace GP_Räknare
     {
         static void Main(string[] args)
         {
+
+        }
+        static void SpeedCalculator(int distanceInterval = 6, int intervals = 3)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            decimal[] times = new decimal[intervals], speeds = new decimal[intervals];
+            for (int i = 0; i < intervals; i++)
+            {
+                stopwatch.Reset();
+                Console.Write("Tap any key to enter interval> ");
+                Console.ReadKey();
+                Console.Clear();
+                stopwatch.Start();
+                Console.Write("Tap any key to enter interval> ");
+                Console.ReadKey();
+                stopwatch.Stop();
+                times[i] = stopwatch.ElapsedMilliseconds;
+            }
         }
     }
 }
