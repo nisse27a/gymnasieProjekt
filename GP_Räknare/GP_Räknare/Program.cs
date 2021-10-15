8e58ea6e2367b1dd1020058d16f52ed0cb18857e
nisse27a <71439623+nisse27a@users.noreply.github.com>
2021-10-15 12:41:00 +0200

    Test

--- GP_Räknare/GP_Räknare/Program.cs
+++ GP_Räknare/GP_Räknare/Program.cs
@@ -11,17 +11,17 @@ namespace GP_Räknare
     {
         static void Main(string[] args)
         {
-
+            SpeedCalculator();
         }
         static void SpeedCalculator(int distanceInterval = 6, int intervals = 3)
         {
             Stopwatch stopwatch = new Stopwatch();
             decimal[] times = new decimal[intervals], speeds = new decimal[intervals];
+            Console.Write("Tap any key to enter interval> ");
+            Console.ReadKey();
             for (int i = 0; i < intervals; i++)
             {
                 stopwatch.Reset();
-                Console.Write("Tap any key to enter interval> ");
-                Console.ReadKey();
                 Console.Clear();
                 stopwatch.Start();
                 Console.Write("Tap any key to enter interval> ");
@@ -29,6 +29,11 @@ namespace GP_Räknare
                 stopwatch.Stop();
                 times[i] = stopwatch.ElapsedMilliseconds;
             }
+            foreach(decimal time in times)
+            {
+                Console.WriteLine(time + "\n");
+            }
+            Console.Read();
         }
     }
 }
